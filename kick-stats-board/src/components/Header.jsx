4b0032b59faf70b0bc6dc239e1a06e93baf3e661
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-01-26 10:07:51 +0530

    style: add header

---
+++ kick-stats-board/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import { FaGithub } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="toolbar">
+        <a href="/" className="branding" aria-label="Go to KickStats home">
+          <img
+            src="https://ik.imagekit.io/q4rna6net/logos/kickstarter_8FV7jGSn6P.png?updatedAt=1737801906550"
+            alt="Kick Stats logo"
+            className="logo"
+          />
+          <span>KickStats</span>
+        </a>
+        <span className="spacer"></span>
+        <a
+          href="https://github.com/SSBarik/saaslabco-frontend-assignment"
+          target="_blank"
+          className="icon-link"
+          aria-label="View KickStats source code on GitHub"
+        >
+          <FaGithub size={30} />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
