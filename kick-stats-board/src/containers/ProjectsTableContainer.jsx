c67871007e71fdcfe675c8df83e66b6fde19d6d3
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-01-26 21:27:51 +0530

    refactor: add project table container

---
+++ kick-stats-board/src/containers/ProjectsTableContainer.jsx
@@ -0,0 +1,28 @@
+import { FETCH_PROJECTS_URL } from "@configs/constant";
+import { projectColumns } from "@configs/columnsConfig";
+import { projectCustomStyles } from "@configs/stylesConfig";
+import { transformProjectData } from "@utils/dataTransformers";
+import useFetchData from "../hooks/useFetchData";
+import PaginatedTable from "@components/common/paginated-table/PaginatedTable";
+
+const ProjectsTableContainer = () => {
+  const {
+    data: projects,
+    isLoading,
+    error,
+  } = useFetchData(FETCH_PROJECTS_URL, transformProjectData);
+
+  return (
+    <div>
+      {error && alert(error)}
+      <PaginatedTable
+        columns={projectColumns}
+        data={projects}
+        isLoading={isLoading}
+        customStyles={projectCustomStyles}
+      />
+    </div>
+  );
+};
+
+export default ProjectsTableContainer;
