afd450f01bb9a026dab2562bed9d502b9fc0dd13
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-01-26 10:14:50 +0530

    style: add home page

--- kick-stats-board/src/App.jsx
+++ kick-stats-board/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ProjectsTable from "./components/ProjectsTable";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <ProjectsTable />
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
---
+++ kick-stats-board/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import ProjectsTable from "../components/ProjectsTable";
+
+const Home = () => {
+  return (
+    <main>
+      <ProjectsTable />
+    </main>
+  );
+};
+
+export default Home;
