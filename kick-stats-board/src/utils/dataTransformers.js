1724779349b2f5f5de693874efdcb2992fac5f95
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-01-26 23:27:36 +0530

    fix: start serial number indexing from 1

--- kick-stats-board/src/utils/dataTransformers.js
+++ kick-stats-board/src/utils/dataTransformers.js
@@ -1,6 +1,6 @@
 export const transformProjectData = (data) => {
-  return data.map((project) => ({
-    sno: project["s.no"],
+  return data.map((project, index) => ({
+    sno: index + 1,
     title: project.title,
     percentageFunded: project["percentage.funded"],
     amountPledged: project["amt.pledged"],
